624a1dd8369b13a2fa51dce85b943e7066a10a51
Wouter van der Beek (wovander) <wovander@cisco.com>
2018-07-04 14:37:59 +0100

    fix

--- install_DeviceBuilder.sh
+++ install_DeviceBuilder.sh
@@ -82,7 +82,7 @@ echo "nano ./iotivity/examples/${code_path}/server.cpp" >> edit_code.sh
 # create the run script
 echo "#!/bin/bash"> run.sh
 #echo "CURPWD=`pwd`">> run.sh
-#echo "CURPWD=$(pwd -P)">> run.sh
+echo "CURPWD=$(pwd -P)">> run.sh
 echo "env LD_LIBRARY_PATH=${CURPWD}/mraa/build/src" >> run.sh
 echo "sudo ldconfig" >> run.sh
 echo "cd ./iotivity/out/linux/${ARCH}/release/examples/${code_path}" >> run.sh
