2fec50967a4584cd9552892b801e10a28b89c85b
Wouter van der Beek (wovander) <wovander@cisco.com>
2018-08-13 15:47:17 +0100

    iotivity2.0 scripts

---
+++ install_IOTivity2.0.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#############################
+#
+# copyright 2018 Open Connectivity Foundation, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#############################
+
+curpwd=`pwd`
+cd ..
+
+# step 1
+sudo apt-get -y install build-essential git scons libtool \
+autoconf valgrind doxygen wget unzip cmake libboost-dev \
+libboost-program-options-dev libboost-thread-dev uuid-dev \
+libexpat1-dev libglib2.0-dev libsqlite3-dev libcurl4-gnutls-dev
+
+# step 2
+git clone https://gerrit.iotivity.org/gerrit/iotivity
+cd iotivity
+git checkout 2.0-rel
+
+# step 3
+git clone https://github.com/01org/tinycbor.git \
+extlibs/tinycbor/tinycbor -b v0.4.1
+
+git clone https://github.com/ARMmbed/mbedtls.git \
+extlibs/mbedtls/mbedtls -b mbedtls-2.4.2
+
+cd $curpwd
